214b9c2e6fa9225290e0d152d716be5192f7e1a8
Thomas Alcala <thomas.alcala@gmail.com>
2014-11-05 22:20:28 -0600

    Flawed concept

--- week4/lab/ModernArtUI/ModernArtUI/src/com/appspot/tomschneider/modernartui/MainActivity.java
+++ week4/lab/ModernArtUI/ModernArtUI/src/com/appspot/tomschneider/modernartui/MainActivity.java
@@ -1,7 +1,11 @@
 package com.appspot.tomschneider.modernartui;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,12 +20,32 @@ import android.graphics.Rect;
 
 public class MainActivity extends Activity {
 
+	RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.main_layout);
+	
+	private int mDisplayWidth, mDisplayHeight;
+	
+	private static final String TAG = "Modern-Art-UI";
+	
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		if (hasFocus) {
+
+			// Get the size of the display so this View knows where borders are
+			mDisplayWidth = relativeLayout.getWidth();
+			mDisplayHeight = relativeLayout.getHeight();
+
+		}
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.main_layout);
 		
+		setContentView(R.layout.activity_main);
+		RectangleView rectangleView1 = new RectangleView(getApplicationContext());
+		relativeLayout.addView(rectangleView1);
+		Log.i(TAG, "Rectangle at " + rectangleView1.getPosition());
 	}
 
 	@Override
@@ -48,30 +72,85 @@ public class MainActivity extends Activity {
 		private final SurfaceHolder mSurfaceHolder;
         private final Paint mPainter = new Paint();
         private Thread mDrawingThread;
+        private final int[] size, position;
+        private final Random r = new Random();
 		
 		
-		public RectangleView(Context context, int width, int height) {
+		public RectangleView(Context context) {
 			super(context);
 			
 			mPainter.setAntiAlias(true);
 
             mSurfaceHolder = getHolder();
             mSurfaceHolder.addCallback(this);
+            
+            size = new int[2];
+            position = new int[2];
+            
+            setSizeAndPosition();
 		}
 		
-		public void draw(Canvas canvas) {
+		private void setSizeAndPosition() {
+			//Check if there are already rectangles, then draw accordingly to occupy all screen space
+			for (int i = 0; i < relativeLayout.getChildCount(); i++) {
+				if(i != 0) {
+					//CHeck if the second rectangle is longer that the first
+				}
+			}
+			
+			int width = r.nextInt(mDisplayWidth * 2 / 3);
+			int height = r.nextInt(mDisplayHeight * 2 / 3);
+			size[0] = width;
+			size[1] = height;
+			
+			int x = 250;
+			int y = 250;
+			position[0] = x;
+			position[1] = y;
+			
+		}
+		
+		public int[] getPosition() {
+			return position;
+		}
+		
+		public int[] getSize() {
+			return size;
+		}
+		
+		@Override
+		public void onDraw(Canvas canvas) {
+			canvas.save();
+			
 			canvas.drawColor(Color.MAGENTA);
-			canvas.drawRect(, mPainter);
+			canvas.drawRect(100.0f, 100.0f, 100.0f, 100.0f, mPainter);
+			
+			canvas.restore();
+		}
+		
+		public void start() {
+			
 		}
 	
 		@Override
 		public void surfaceCreated(SurfaceHolder holder) {
-		
+			mDrawingThread = new Thread(new Runnable() {
+				public void run() {
+					Canvas canvas = null;
+					while(!Thread.currentThread().isInterrupted()) {
+						canvas = mSurfaceHolder.lockCanvas();
+						if(null != canvas) {
+							draw(canvas);
+							mSurfaceHolder.unlockCanvasAndPost(canvas);
+						}
+					}
+				}
+			});
 		}
 	
 		@Override
 		public void surfaceDestroyed(SurfaceHolder holder) {
-		
+			mDrawingThread.interrupt();
 		}
 
 		@Override
