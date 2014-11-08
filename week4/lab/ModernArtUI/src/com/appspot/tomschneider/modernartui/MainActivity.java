9b02a376afd7002cd0e5e1026a7c703213223218
Thomas Alcala <thomas.alcala@gmail.com>
2014-11-08 00:15:54 -0600

    Display rectangles

--- week4/lab/ModernArtUI/src/com/appspot/tomschneider/modernartui/MainActivity.java
+++ week4/lab/ModernArtUI/src/com/appspot/tomschneider/modernartui/MainActivity.java
@@ -1,22 +1,33 @@
 package com.appspot.tomschneider.modernartui;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
+	
+	private static final String TAG = "ModernArtUI";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main);
+		RelativeLayout layout = (RelativeLayout) findViewById(R.id.layout);
 		
-		RectangleSurfaceView mySurface=new RectangleSurfaceView(this);
-	    setContentView(mySurface);
+		RectangleSurfaceView mSurface = new RectangleSurfaceView(getApplicationContext());
+		layout.addView(mSurface);
 	}
 
 	@Override
@@ -40,14 +51,41 @@ public class MainActivity extends Activity {
 	
 	private class RectangleSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
 
+		private final Paint mPainter = new Paint();
+		private SurfaceHolder mSurfaceHolder; 
+		private Thread rectangleThread;
+		private DisplayMetrics mDisplay;
+		private int mDisplayWidth, mDisplayHeight;
+		
 		public RectangleSurfaceView(Context context) {
 			super(context);
 			// TODO Auto-generated constructor stub
+			
+			mDisplay = new DisplayMetrics();
+	        MainActivity.this.getWindowManager().getDefaultDisplay().getMetrics(mDisplay);
+	        mDisplayWidth = mDisplay.widthPixels;
+	        mDisplayHeight = mDisplay.heightPixels;
+	        
+			mSurfaceHolder = getHolder();
+			mSurfaceHolder.addCallback(this);
 		}
 
 		@Override
 		public void surfaceCreated(SurfaceHolder holder) {
 			// TODO Auto-generated method stub
+			//this.mSurfaceHolder = holder;
+			
+			rectangleThread = new Thread( new Runnable() {
+				public void run() {
+						Canvas canvas = null;
+						canvas = mSurfaceHolder.lockCanvas();
+						if (null != canvas) {
+							drawRectangles(canvas);
+							mSurfaceHolder.unlockCanvasAndPost(canvas);
+						}
+				}
+			});
+			rectangleThread.start();
 			
 		}
 
@@ -55,13 +93,42 @@ public class MainActivity extends Activity {
 		public void surfaceChanged(SurfaceHolder holder, int format, int width,
 				int height) {
 			// TODO Auto-generated method stub
+			mDisplay = new DisplayMetrics();
+	        MainActivity.this.getWindowManager().getDefaultDisplay().getMetrics(mDisplay);
+	        mDisplayWidth = mDisplay.widthPixels;
+	        mDisplayHeight = mDisplay.heightPixels;
 			
 		}
 
 		@Override
 		public void surfaceDestroyed(SurfaceHolder holder) {
 			// TODO Auto-generated method stub
-			
+			rectangleThread.interrupt();
+		}
+		
+		public void drawRectangles(Canvas canvas) {
+			canvas.drawColor(Color.DKGRAY);
+			mPainter.setStyle(Paint.Style.FILL_AND_STROKE);
+            mPainter.setColor(getResources().getColor(android.R.color.darker_gray));
+            RectF rect = new RectF(0, 0, mDisplayWidth / 2, mDisplayHeight / 3);
+            Log.i(TAG, "Drawing rectangle 1");
+            canvas.drawRect(rect, mPainter);
+            mPainter.setColor(getResources().getColor(android.R.color.holo_blue_dark));
+            rect = new RectF(0, mDisplayHeight / 3, mDisplayWidth / 2, mDisplayHeight * 2 / 3);
+            canvas.drawRect(rect, mPainter);
+            mPainter.setColor(getResources().getColor(android.R.color.holo_orange_dark));
+            rect = new RectF(0, mDisplayHeight * 2 / 3, mDisplayWidth / 2, mDisplayHeight);
+            canvas.drawRect(rect, mPainter);
+            
+            mPainter.setColor(getResources().getColor(android.R.color.holo_green_dark));
+            rect = new RectF(mDisplayWidth / 2, 0, mDisplayWidth, mDisplayHeight / 3);
+            canvas.drawRect(rect, mPainter);
+            mPainter.setColor(getResources().getColor(android.R.color.holo_red_dark));
+            rect = new RectF(mDisplayWidth / 2, mDisplayHeight / 3, mDisplayWidth, mDisplayHeight * 2 / 3);
+            canvas.drawRect(rect, mPainter);
+            mPainter.setColor(getResources().getColor(android.R.color.holo_purple));
+            rect = new RectF(mDisplayWidth / 2, mDisplayHeight * 2 / 3, mDisplayWidth, mDisplayHeight);
+            canvas.drawRect(rect, mPainter);
 		}
 
 	}
