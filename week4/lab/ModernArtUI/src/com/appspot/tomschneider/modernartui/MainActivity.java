ac9230016cb4460bf524388e3e5aef7d0f6591e8
Thomas Alcala <thomas.alcala@gmail.com>
2014-11-09 16:05:30 -0600

    little progress

--- week4/lab/ModernArtUI/src/com/appspot/tomschneider/modernartui/MainActivity.java
+++ week4/lab/ModernArtUI/src/com/appspot/tomschneider/modernartui/MainActivity.java
@@ -1,5 +1,7 @@
 package com.appspot.tomschneider.modernartui;
 
+import java.math.BigDecimal;
+
 import android.animation.Animator;
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -9,6 +11,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
@@ -26,6 +29,14 @@ public class MainActivity extends Activity {
 	private static final String TAG = "ModernArtUI";
 	
 	private SeekBar seekBar = null;
+	private float alpha;
+	private final Handler mHandler = new Handler();
+	
+	/*final Runnable mUpdateAlpha = new Runnable() {
+		public void run() {
+			updateAlphaInUI();
+		}
+	};*/
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,15 +54,13 @@ public class MainActivity extends Activity {
 		
 		seekBar.setVisibility(SeekBar.VISIBLE);
 		seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-	          float progress = 0;
+	          int progress = 0;
 	           
 	          @Override
 	          public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
 	              progress = progresValue;
 	              //Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
-	              mSurface.setAlpha(progress / 100);
-	              Log.i(TAG, "Setting alpha: " + (progress / 100));
-	              
+	              //changeAlpha(mSurface, progress);
 	          }
 	         
 	          @Override
@@ -63,9 +72,37 @@ public class MainActivity extends Activity {
 	          public void onStopTrackingTouch(SeekBar seekBar) {
 	              //textView.setText("Covered: " + progress + "/" + seekBar.getMax());
 	              Toast.makeText(getApplicationContext(), "Stopped tracking seekbar, progress: " + progress, Toast.LENGTH_SHORT).show();
+	              changeAlpha(mSurface, progress);
 	          }
 	       });
 	}
+	
+	public static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);       
+        return bd.floatValue();
+    }
+	
+	public void changeAlpha(final RectangleSurfaceView view, final int progress) {
+		//Thread alphaThread = new Thread(new Runnable() {
+			//public void run() {
+				alpha = 1 - ((float) progress / 100);
+				alpha = round(alpha, 1);
+				//Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+				Log.i(TAG, "Alpha: " + alpha);
+				//view.getBackground().setAlpha(alpha);
+				view.animate().alpha(alpha).setDuration(30000);
+				//view.animate().alpha(1f).setDuration(3000);
+				Log.i(TAG, "Progress: " + progress + ".Setting alpha: " + alpha);
+			//}
+		//});
+		//alphaThread.start();
+	}
+	
+	/*private void updateAlphaInUI() {
+		view.setAlpha(alpha);
+		view.postInvalidate();
+	}*/
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
