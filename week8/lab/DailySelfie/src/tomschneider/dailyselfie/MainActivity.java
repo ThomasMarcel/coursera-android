fc951927a131fc7a08ef0e9d25123566a38d4270
Thomas Alcala <thomas.alcala@gmail.com>
2014-11-14 23:02:03 -0600

    Some progress, still crashing on cam intent result

--- week8/lab/DailySelfie/src/tomschneider/dailyselfie/MainActivity.java
+++ week8/lab/DailySelfie/src/tomschneider/dailyselfie/MainActivity.java
@@ -2,30 +2,48 @@ package tomschneider.dailyselfie;
 
 import android.app.Activity;
 import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.os.Build;
 import android.provider.MediaStore;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
 	static final int REQUEST_IMAGE_CAPTURE = 1;
+	
+	int displayWidth, displayHeight;
+	ImageView mPictureTakenView;
+	
+	static Context mContext;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		mContext = getApplicationContext();
+		
 		setContentView(R.layout.activity_main);
 		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
+			Fragment placeHolderFragment = new PlaceholderFragment();
+			FragmentManager fragmentManager = getSupportFragmentManager();
+			fragmentManager.beginTransaction().add(placeHolderFragment, null).commit();
 		}
 	}
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,7 +58,8 @@ public class MainActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_picture) {
+			dispatchTakePictureIntent();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -63,10 +82,41 @@ public class MainActivity extends Activity {
 		}
 	}
 	
+	public static class PictureFragment extends Fragment {
+		Bitmap bmp;
+		
+		public PictureFragment(Bitmap bmp) {
+			this.bmp = bmp;
+		}
+		
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			View rootView = new View(mContext);
+			Canvas canvas = null;
+			// TODO - Resize bitmap to screen size
+			
+			canvas.drawBitmap(bmp, 0, 0, null);
+			rootView.draw(canvas);
+			return rootView;
+		}
+	}
+	
 	private void dispatchTakePictureIntent() {
 	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
 	        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+	    }
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+	        Bundle extras = data.getExtras();
+	        Bitmap imageBitmap = (Bitmap) extras.get("data");
+	        mPictureTakenView.setImageBitmap(imageBitmap);
+	        Fragment picFragment = new PictureFragment(imageBitmap);
+	        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+	        transaction.replace(R.id.container, picFragment);
 	    }
 	}
 }
