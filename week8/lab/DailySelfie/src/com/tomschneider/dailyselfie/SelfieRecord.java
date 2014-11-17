7f6a9977577ec70a7e68e248d7fc03dd7dca4a69
Thomas Alcala <thomas.alcala@gmail.com>
2014-11-16 21:59:45 -0600

    New take on DailySelfie

---
+++ week8/lab/DailySelfie/src/com/tomschneider/dailyselfie/SelfieRecord.java
@@ -0,0 +1,45 @@
+package com.tomschneider.dailyselfie;
+
+import java.util.Date;
+
+import android.graphics.Bitmap;
+
+public class SelfieRecord {
+	private Bitmap mBmp;
+	private String mName;
+	private Date mDate;
+	
+	public SelfieRecord(Bitmap bmp, String name) {
+		this.mBmp = bmp;
+		this.mName = name;
+		this.mDate = new Date();
+	}
+	
+	public SelfieRecord() {
+		this.mDate = new Date();
+	}
+	
+	public void setBmp(Bitmap bmp) {
+		this.mBmp = bmp;
+	}
+	
+	public Bitmap getBmp() {
+		return this.mBmp;
+	}
+	
+	public void setDate(Date date) {
+		this.mDate = date;
+	}
+	
+	public Date getDate() {
+		return this.mDate;
+	}
+	
+	public void setName(String name) {
+		this.mName = name;
+	}
+	
+	public String getName() {
+		return this.mName;
+	}
+}
